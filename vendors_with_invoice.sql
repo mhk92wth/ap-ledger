1dd0f463290245d9fd4e3db687a64be141213202
mhk92wtf <mhkamari92@gmail.com>
2026-08-23 22:53:38 +0330

    maximum amount for general ledger accouonts

--- vendors_with_invoice.sql
+++ vendors_with_invoice.sql
@@ -2,7 +2,10 @@ select
     v.vendor_id as id,
     v.vendor_name as 'vendor name',
     count(i.invoice_id) as 'number of invoices',
-    sum(i.invoice_total - i.credit_total - i.payment_total) as 'sum of invoices'
+    sum(i.invoice_total) as 'total invoice',
+    sum(i.payment_total) as 'total payment',
+    sum(i.credit_total) as 'total credit',
+    sum(i.invoice_total - i.credit_total - i.payment_total) as 'outstanding'
 from vendors as v inner join invoices as i using (vendor_id)
 where i.invoice_total - i.credit_total - i.payment_total > 0
 group by v.vendor_id 
